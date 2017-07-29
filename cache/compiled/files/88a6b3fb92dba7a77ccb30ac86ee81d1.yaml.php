2868730427c6a93ba54ee727d6690c258c8d8d56
Mugo Petersen <mugpet@gmail.co>
2017-07-29 11:30:06 +0200

    updated email plugin

--- cache/compiled/files/88a6b3fb92dba7a77ccb30ac86ee81d1.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => 'themes://landio/landio.yaml',
-    'modified' => 1501313544,
-    'data' => [
-        'enabled' => true
-    ]
-];
